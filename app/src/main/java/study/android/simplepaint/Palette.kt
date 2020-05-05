965ca04ea013926d585664b35a99de50a1a0c9a5
Vladimir Ilyin <oivt@ya.ru>
2020-05-05 19:46:39 +0300

    Initial commit

---
+++ app/src/main/java/study/android/simplepaint/Palette.kt
@@ -0,0 +1,39 @@
+package study.android.simplepaint
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Paint
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+
+
+object Palette {
+    var paint = Paint()
+
+    fun setColor(color: Int) {
+        paint.color = color
+    }
+
+    fun setSize(size: Int) {
+        paint.strokeWidth = size.toFloat()
+    }
+
+    fun encodeToBase64(image: Bitmap): String {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        val b: ByteArray = baos.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
+
+    fun decodeBase64(input: String?): Bitmap {
+        val decodedByte: ByteArray = Base64.decode(input, 0)
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size)
+    }
+
+    init {
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 5f
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+    }
+}
